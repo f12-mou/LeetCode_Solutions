ab3fed601291a7c3ad3483b632b82ed36e516645
f12-mou <faria12mou@gmail.com>
2025-01-14 01:50:44 +0600

    topsort added

---
+++ Neet_150_Solves/code_207_course_schedule.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+public:
+	bool vis[20005];
+
+	bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+	{
+		int n = numCourses;
+
+		map<int, int> mp;
+		map<int, vector<int>> hehe;
+
+		for (int i = 0; i < prerequisites.size(); i++)
+		{
+			int a = prerequisites[i][0];
+			int b = prerequisites[i][1];
+			mp[b]++;
+			hehe[a].push_back(b);
+			cout << "b: " << b << " is pushed into : " << a << endl;
+		}
+		queue<int> q;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (mp[i] == 0)
+			{
+				q.push(i);
+			}
+		}
+		while (q.empty() == false)
+		{
+			int now = q.front();
+			q.pop();
+			vis[now] = true;
+
+			for (int i = 0; i < hehe[now].size(); i++)
+			{
+				int who = hehe[now][i];
+				mp[who]--;
+				cout << "now who: " << now << " " << who << endl;
+				if (mp[who] == 0)
+				{
+					q.push(who);
+				}
+			}
+		}
+		bool ok = true;
+		for (int i = 0; i < n; i++)
+		{
+			if (vis[i] == false)
+			{
+				ok = false;
+				break;
+			}
+		}
+		return ok;
+	}
+};
